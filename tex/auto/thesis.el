678efa40488522cb2aea2705d87cfe26570d3227
Tomas Krizek <tomas.krizek@mailbox.org>
2016-01-09 19:03:52 +0100

    tulthesis template

---
+++ tex/auto/thesis.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "thesis"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("tulthesis" "FM" "DP")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("babel" "czech") ("inputenc" "utf8")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "tulthesis"
+    "tulthesis10"
+    "babel"
+    "inputenc")))
+
